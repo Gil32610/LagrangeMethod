a4abc328d8d43322cfdfed9ee4747baabc7f6b2c
GIL32610 <gil.00000032610@unicap.br>
2023-05-05 12:03:40 -0300

    Problem1 and Problem2 exercises

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ public class App {
         question1();
         question2();
         question3();
+        problem1();
+        problem2();
     }
 
-    public static void questionIndex(int example, double valueInterp, double[] x, double[] y) {
-        System.out.printf("Question %d: Calculate L2(%f) using the following data:\n", example, valueInterp);
+    public static void questionIndex(int example, double valueInterp, double[] x, double[] y, int m) {
+        System.out.printf("Question %d: Calculate L%d(%.3f) using the following data:\n", example, m, valueInterp);
         System.out.println("Abscissa:");
         printVector(x);
         System.out.println("Ordinate:");
@@ -33,7 +35,7 @@ public class App {
         double[] x = { 0.1, 0.6, 0.8 };
         double[] y = { 1.221, 3.32, 4.953 };
         double z = 0.2;
-        questionIndex(1, z, x, y);
+        questionIndex(1, z, x, y, m - 1);
         printResult(m - 1, z, LagrangeMethod.interpolation(m, x, y, z));
     }
 
@@ -42,7 +44,7 @@ public class App {
         double[] x = { 1, 1.3, 1.7, 2.0 };
         double[] y = { 0.8415, 1.2526, 1.6858, 1.8186 };
         double z = 1.2;
-        questionIndex(2, z, x, y);
+        questionIndex(2, z, x, y, m - 1);
 
         printResult(m - 1, z, LagrangeMethod.interpolation(m, x, y, z));
     }
@@ -52,11 +54,33 @@ public class App {
         double[] x = { .1, .6 };
         double[] y = { 1.221, 3.32 };
         double z = .2;
-        questionIndex(3, z, x, y);
+        questionIndex(3, z, x, y, m - 1);
         printResult(m - 1, z, LagrangeMethod.interpolation(m, x, y, z));
     }
 
     public static void printResult(int m, double z, double interpolation) {
-        System.out.printf("Interpolation result: L%d(%.2f) = %.3f\n\n", m, z, interpolation);
+        System.out.printf("Interpolation result: L%d(%.2f) = %.4f\n\n", m, z, interpolation);
+    }
+
+    public static void problem1() {
+
+        int m = 5;
+        double[] x = { 200.0, 220.0, 240., 260., 280. };
+        double[] y = { 1.075, 1.102, 1.136, 1.183, 1.250 };
+        double z = 250.0;
+        questionIndex(4, z, x, y, m - 1);
+        printResult(m - 1, z, LagrangeMethod.interpolation(m, x, y, z));
+    }
+
+    public static void problem2() {
+        int m = 7;
+        double[] x = { 0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0 };
+        double[] y = { 0.5, 0.69146, 0.84134, 0.93319, 0.97725, 0.99379, 0.99865 };
+        double z = 0.3;
+        for (int i = 1; i < m; i++) {
+            questionIndex(4, z, x, y, i);
+            printResult(i, z, LagrangeMethod.interpolation(i, x, y, z));
+        }
+
     }
 }
